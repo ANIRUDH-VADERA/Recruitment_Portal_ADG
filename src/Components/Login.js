054e59fd23e75fa19db75e09716fef8eb97a2176
pranavvdesai <pranavdesai.psd@gmail.com>
2022-01-21 02:18:45 +0530

    refactor: everything

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,60 +1,112 @@
-import React from 'react';
-import "./Login.css"
-import back_img from "../images/back_img.svg"
-import {useState} from "react";
-import {useNavigate} from "react-router-dom";
+/* eslint-disable camelcase */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable react/button-has-type */
+import React, { useState } from "react";
+import "./Login.css";
+import { useNavigate } from "react-router-dom";
+import back_img from "../images/back_img.svg";
 
 function Login() {
-  
-  const [isOtp ,setIsOtp] = useState(0);
+  const [isOtp, setIsOtp] = useState(0);
 
   function handleClick() {
     setIsOtp(1);
   }
-  
+
   const navigate = useNavigate();
 
-  return (  
-    <>
-    {isOtp ? 
-    <div className='otp_page_login'>
-      <div className="left">
-        <div className="main_form">
+  return (
+    <div>
+      {isOtp ? (
+        <div className="otp_page_login">
+          <div className="left">
+            <div className="main_form">
               <h1 className="heading">One Time Password</h1>
-              <p className='para'>Check your VIT Mail Inbox or Spam Folder for the OTP</p>
+              <p className="para">
+                Check your VIT Mail Inbox or Spam Folder for the OTP
+              </p>
               <form className="form">
-                  <label for="fname">OTP</label>
-                  <input type="text" id="name" name="name" placeholder="Enter the name" />
-                  <p className="bottom">Didn’t recieve OTP?  <span className="resendOTP" onClick={()=>{}}> Resend OTP </span></p>
+                <label htmlFor="fname">OTP</label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="Enter the name"
+                />
+                <p className="bottom">
+                  Didn’t recieve OTP?{" "}
+                  <span className="resendOTP" onClick={() => {}}>
+                    {" "}
+                    Resend OTP{" "}
+                  </span>
+                </p>
               </form>
-              <button className="btn1" onClick={()=>{navigate("/aboutyou")}} >Verify OTP</button>
-              <p className="tosignup"  onClick={()=>{setIsOtp(0)}}>Go Back</p>
+              <button
+                className="btn1"
+                onClick={() => {
+                  navigate("/aboutyou");
+                }}
+              >
+                Verify OTP
+              </button>
+              <p
+                className="tosignup"
+                onClick={() => {
+                  setIsOtp(0);
+                }}
+              >
+                Go Back
+              </p>
+            </div>
           </div>
-      </div>
-      <div className="right">
-        <img alt="background" src={back_img}/>
-      </div>
-  </div>        
-    :
-    <div className='login_page'>
-      <div className="left">
-        <div className="main_form">
+          <div className="right">
+            <img alt="background" src={back_img} />
+          </div>
+        </div>
+      ) : (
+        <div className="login_page">
+          <div className="left">
+            <div className="main_form">
               <h1 className="heading">Login</h1>
-              <p className='para'>Login with your VIT Registration Number</p>
+              <p className="para">Login with your VIT Registration Number</p>
               <form className="form">
-                  <label for="fname">Registration Number</label>
-                  <input type="text" id="name" name="name" placeholder="Enter your Registration Number. Eg: 20BCE0001" />
+                <label htmlFor="fname">Registration Number</label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="Enter your Registration Number. Eg: 20BCE0001"
+                />
               </form>
-              <button className="btn1" onClick={()=>{handleClick()}} >Login with OTP</button>
-              <p className="bottom">Don’t have an account? <span className="tosignup" onClick={()=>{navigate("/SignUp")}}>  Create One</span></p>
+              <button
+                className="btn1"
+                onClick={() => {
+                  handleClick();
+                }}
+              >
+                Login with OTP
+              </button>
+              <p className="bottom">
+                Don’t have an account?{" "}
+                <span
+                  className="tosignup"
+                  onClick={() => {
+                    navigate("/SignUp");
+                  }}
+                >
+                  {" "}
+                  Create One
+                </span>
+              </p>
+            </div>
           </div>
-      </div>
-      <div className="right">
-        <img alt="background" src={back_img}/>
-      </div>
+          <div className="right">
+            <img alt="background" src={back_img} />
+          </div>
+        </div>
+      )}
     </div>
-    }
-    </>
   );
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import adg_logo from "../images/adg_logo.svg"
-import './Navbar.css';
+/* eslint-disable camelcase */
+import React from "react";
+import adg_logo from "../images/adg_logo.svg";
+import "./Navbar.css";
 
 function Navbar() {
-    return (
-        <div className="navbar">
-            <img alt = "Adg_Logo" src = {adg_logo} />
-        </div>
-    )
+  return (
+    <div className="navbar">
+      <img alt="Adg_Logo" src={adg_logo} />
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
